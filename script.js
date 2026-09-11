22eb82304b915f119c2fbe27b1438129d2998900
Anshu kumari <anshuak6595@gmail.com>
2026-09-11 21:28:48 +0530

    Add js

---
+++ script.js
@@ -0,0 +1,49 @@
+const filter = {
+  brightness: {
+    value: 100,
+    min: 0,
+    max: 200,
+    unit: "%",
+  },
+  contrast: {
+    value: 100,
+    min: 0,
+    max: 200,
+    unit: "%",
+  },
+  exposture: {
+    value: 100,
+    min: 0,
+    max: 200,
+    unit: "%",
+  },
+  saturation: {
+    value: 100,
+    min: 0,
+    max: 200,
+    unit: "%",
+  },
+  hueRotate: {
+    value: 0,
+    min: 0,
+    max: 360,
+    unit:"deg",
+  },
+  blur:{
+    value:0,
+    min:0,
+    max:20,
+    unit:"px",
+  },
+  grayScale :{
+    value:0,
+    min:0,
+    max:100,
+    unit:"%"
+  },
+  sepia:{
+
+  },
+  opacity:100,
+  invert:0,
+};
